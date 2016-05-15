ec51cbeaab70f4143b03a212885fcc9334192da7
Naresh Damji <Naresh Damji>
2016-05-15 14:56:59 +0530

    Modified data some looks

--- DB_con.js
+++ DB_con.js
@@ -3,16 +3,17 @@ var connection = new ActiveXObject("adodb.connection") ;
 var connectionstring="Provider=SQLOLEDB;Data Source=naresh\\naresh_server;Trusted_Connection=Yes;Database=test_db;"
 connection.Open(connectionstring);
 var rs = new ActiveXObject("ADODB.Recordset");
-
+var total =0
 rs.Open("SELECT * FROM expense_data where category='Income'", connection);
 rs.MoveFirst
 while(!rs.eof)
 {
 	document.write("<tr>");
-   document.write("<td>" + rs.fields(2) + "</td>");
+   document.write("<td>" + rs.fields(1) + "</td>" + "<td>" + rs.fields(2) + "</td>");
    document.write("</tr>");
+   total=total+ rs.fields(2);
    rs.movenext;
 }
-
+document.write("<tr><td>Total Amount</td><td>" + total + "</td></tr>");
 rs.close;
 connection.close;
